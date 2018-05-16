088575a5632faac59ab495d67be9cf4c045b784e
suvorovroman <suvorovroman@gmail.com>
2018-05-17 01:45:26 +0300

    Черновик основного экрана

--- app/src/main/java/ru/finfly/myapplication/SendHtmlRequest.kt
+++ app/src/main/java/ru/finfly/myapplication/SendHtmlRequest.kt
@@ -9,6 +9,8 @@ import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.*
 import android.widget.SearchView
@@ -17,8 +19,10 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import android.widget.TextView
+import com.android.volley.toolbox.JsonArrayRequest
 
 import kotlinx.android.synthetic.main.activity_send_html_request.*
+import org.json.JSONArray
 
 class SendHtmlRequest : AppCompatActivity() {
 
@@ -39,29 +43,68 @@ class SendHtmlRequest : AppCompatActivity() {
                     }
                 }
         )
+
+        findViewById<RecyclerView>(R.id.recyclerView).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = Adapter()
+        }
+    }
+
+    var data = JSONArray()
+
+    private inner class Adapter:RecyclerView.Adapter<RecyclerView.ViewHolder>()
+    {
+        inner class ViewHolder(view: View):RecyclerView.ViewHolder(view)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return ViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                            R.layout.stock_view_holder,
+                            parent,
+                            false
+                    )
+            )
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder.itemView.findViewById<TextView>(R.id.textView).text = data.optString(position, "available")
+        }
+
+        override fun getItemCount() = data.length()
     }
 
     override fun onStart()
     {
         super.onStart()
-
-        val text_view = findViewById<TextView>(R.id.editText)
-
-        val queue = Volley.newRequestQueue(this)
-        val url = "http://95.165.143.36/finfly/lua.exe?hello.lua"
-
-        val request = StringRequest(Request.Method.GET, url,
-                Response.Listener<String> { response -> Log.d("Response", response); text_view.text = response },
-                Response.ErrorListener { Log.d("Error", "Error") })
-
-        queue.add(request)
-
         if(Intent.ACTION_SEARCH.equals(intent.action))
         {
-            var query = intent.getStringExtra(SearchManager.QUERY)
-            Log.d("Query:", query)
+            Volley.newRequestQueue(this).add(
+                    JsonArrayRequest(
+                            com.android.volley.Request.Method.GET,
+                            "http://95.165.143.36/finfly/lua.exe?stock.lua%20"+
+                                    "\"" + intent.getStringExtra(SearchManager.QUERY) + "\"",
+                            null,
+                            Response.Listener<JSONArray>
+                            {
+                                data = it
+                                findViewById<RecyclerView>(R.id.recyclerView).adapter.notifyDataSetChanged()
+                            },
+                            Response.ErrorListener {
+                                AlertDialog.Builder(this).apply {
+                                    setTitle(R.string.title_volley_error)
+                                    setMessage(it.toString())
+                                    setIcon(R.drawable.ic_error_outline_black_24dp)
+                                    setCancelable(false)
+                                    setPositiveButton(
+                                            R.string.ok_button_label,
+                                            {dialog, _ -> dialog.cancel()}
+                                    )
+                            }.show()
+                        }
+                )
+            )
         }
-  }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         getMenuInflater().inflate(R.menu.options_menu, menu)
